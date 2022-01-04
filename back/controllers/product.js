697e635178394f9999422624eb356a56d25a60af
Ebony Jackson <travelbeanie@gmail.com>
2022-01-04 17:45:40 -0500

    B Update

--- back/controllers/product.js
+++ back/controllers/product.js
@@ -1,66 +1,12 @@
 const uuid = require('uuid/v1');
 const Product = require('../models/Product');
 
-let url = window.location.href;
-urlObj = new URL(url);
-let id = urlObj.searchParams.get('id');
-
-console.log(id);
-
-
-fetch( 'http://localhost:3000/api/products/'+id)
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data);
-
-          let productImg = document.getElementsByClassName('item_img')[0];
-
-          productImg.innerHTML = `<img src="${data.imageUrl}" /`
-
-          let title = document.getElementsByClassName('title');
-          title.innerHTML = data.name;
-          
-
-          let colors = document.getElementById('colors');
-          data.colors.forEach(color =>
-            options +=`<option value"${color}"> Please, select a color --</option>`)
-
-        
-            let prices = document.getElementsByClassName('item__content__titlePrice')[0];
-            title.innerHTML = data.price;
-
-            let description = document.getElementsByClassName('item__content__description');
-            title.innerHTML = data.description;
-
-            
-      });
-
-      colors.innerHtml += options;
-
-
-      
-      
-      productsDiv.innerHTML = products;
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-
 exports.getAllProducts = (req, res, next) => {
   Product.find().then(
     (products) => {
       const mappedProducts = products.map((product) => {
         product.imageUrl = req.protocol + '://' + req.get('host') + '/images/' + product.imageUrl;
-         return product;
+        return product;
       });
       res.status(200).json(mappedProducts);
     }
@@ -87,16 +33,20 @@ exports.getOneProduct = (req, res, next) => {
   )
 };
 
-
- 
-  
- 
-  //products: [string] <-- array of product _id
- 
-  
-
-
-  exports.orderProducts = (req, res, next) => {
+/**
+ *
+ * Expects request to contain:
+ * contact: {
+ *   firstName: string,
+ *   lastName: string,
+ *   address: string,
+ *   city: string,
+ *   email: string
+ * }
+ * products: [string] <-- array of product _id
+ *
+ */
+exports.orderProducts = (req, res, next) => {
   if (!req.body.contact ||
       !req.body.contact.firstName ||
       !req.body.contact.lastName ||
@@ -124,9 +74,8 @@ exports.getOneProduct = (req, res, next) => {
       )
     });
     queries.push(queryPromise);
-  }}
-  
-   Promise.all(queries).then(
+  }
+  Promise.all(queries).then(
     (products) => {
       const orderId = uuid();
       return res.status(201).json({
@@ -139,4 +88,5 @@ exports.getOneProduct = (req, res, next) => {
     (error) => {
       return res.status(500).json(new Error(error));
     }
-  )
+  );
+};
--- front/js/product.js
+++ front/js/product.js
@@ -1,4 +1,14 @@
-fetch( 'http://localhost:3000/api/products/')
+const uuid = require('uuid/v1');
+const Product = require('../models/Product');
+
+let url = window.location.href;
+urlObj = new URL(url);
+let id = urlObj.searchParams.get('id');
+
+console.log(id);
+
+
+fetch( 'http://localhost:3000/api/products/'+id)
   .then(
     function(response) {
       if (response.status !== 200) {
@@ -7,34 +17,40 @@ fetch( 'http://localhost:3000/api/products/')
         return;
       }
 
+      // Examine the text in the response
       response.json().then(function(data) {
-        consle.log(data);
+        console.log(data);
+
+          let productImg = document.getElementsByClassName('item_img')[0];
+
+          productImg.innerHTML = `<img src="${data.imageUrl}" /`
+
+          let title = document.getElementsByClassName('title');
+          title.innerHTML = data.name;
+          
+
+          let colors = document.getElementById('colors');
+          data.colors.forEach(color =>
+            options +=`<option value"${color}"> Please, select a color --</option>`)
+
+        
+            let prices = document.getElementsByClassName('item__content__titlePrice')[0];
+            title.innerHTML = data.price;
+
+            let description = document.getElementsByClassName('item__content__description');
+            title.innerHTML = data.description;
+
+            
       });
+
+      colors.innerHtml += options;
+
+
+      
+      
+      productsDiv.innerHTML = products;
     }
   )
   .catch(function(err) {
     console.log('Fetch Error :-S', err);
   });
-
-  const { foo, bar }  = await iAmAPromise.then(result => result.data);
-
-  function status(response) {
-    if (response.status >= 200 && response.status < 300) {
-      return Promise.resolve(response)
-    } else {
-      return Promise.reject(new Error(response.statusText))
-    }
-  }
-  
-  function json(response) {
-    return response.json()
-  }
-  
-  fetch('users.json')
-    .then(status)
-    .then(json)
-    .then(function(data) {
-      console.log('Request succeeded with JSON response', data);
-    }).catch(function(error) {
-      console.log('Request failed', error);
-    });
