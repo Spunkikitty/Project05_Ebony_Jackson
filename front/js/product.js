485015c41e23f46c212e1c910dd549aab1b15663
Ebony Jackson <travelbeanie@gmail.com>
2022-02-11 16:52:38 -0500

    update D

--- front/js/product.js
+++ front/js/product.js
@@ -1,10 +1,11 @@
+// Retrieve the product id
 let url = window.location.href;
 urlObj = new URL(url);
 let id = urlObj.searchParams.get('id');
 
 console.log(id);
 
-
+//retrieve the single product from the api
 fetch( 'http://localhost:3000/api/products/'+id)
   .then(
     function(response) {
@@ -17,42 +18,101 @@ fetch( 'http://localhost:3000/api/products/'+id)
       // Examine the text in the response
       response.json().then(function(data) {
         console.log(data);
+        //modify the DOM
+        let productImg = document.getElementsByClassName('item__img')[0];
 
-          let productImg = document.getElementsByClassName('item_img')[0];
+        productImg.innerHTML = `<img src="${data.imageUrl}"/>`;
 
-          productImg.innerHTML = `<img src=${data.imageUrl} />`;
-
-          let title = document.getElementById('title');
-          title.innerHTML = data.name;
-          
-
-          let colors = document.getElementById('colors');
-          
-          let options ='';
-          data.colors.forEach(color => {
-            options +=` <option value="${color}">${color}"</option>`;
-          });
-
-          colors.innerHtml += options;
+        let title = document.getElementById('title');
+        title.innerHTML = data.name;
 
 
-            let prices = document.getElementsByClassName('item__content__titlePrice')[0];
-            title.innerHTML = data.price;
+        let colors = document.getElementById('colors');
 
-            let description = document.getElementsByClassName('item__content__description');
-            title.innerHTML = data.description;
+        let options ='';
+        data.colors.forEach(color => {
+            options +=` <option value="${color}">${color}</option>`;
+        });
+    
+        colors.innerHTML += options;
 
-            
-      });
+        let prices = document.getElementById('price');
+         prices.innerHTML = data.price;
 
-      
+        let description = document.getElementById('description');
+         description.innerHTML = data.description;
 
+        });
 
-      
-      
-      productsDiv.innerHTML = products;
-    }
-  )
+ }
+  )  
   .catch(function(err) {
     console.log('Fetch Error :-S', err);
   });
+
+
+//  if (sessionStorage.clickcount) {
+  //  sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
+  //} else {
+    //sessionStorage.clickcount = 1;
+  //}
+  //document.getElementById("result").innerHTML = "You have clicked the button " +
+  //sessionStorage.clickcount + " time(s) in this session.";
+
+
+
+  document.getElementById("addToCart").addEventListener("click", () => {
+  
+    alert("adding product");
+  });
+
+  // cart can be made as an array  containing 3 things product Id, quanity of product, 
+  //  -color of the product 
+
+  //check if there is no existing value 
+
+  let cart = localStorage.getItem('cart');
+  color = document.getElementById('colors').value; 
+  let quantity = document.getElementById("quantity").value
+
+  if(color == ""){
+     alert("please select a color")
+  }else{
+    let products = [];
+    if(cart==null){
+  
+      products.push({_id:id, quantity:parseInt(quantity), color:'blue'})
+      localStorage.setItem('cart',JSON.stringify(products));
+    }
+    
+    else{
+      //item is already existing in the cart
+      cart = JSON.parse(localStorage.getItem('cart'));   
+      //Y- retrieve array and specific item then, check color then 
+      let index = cart.findIndex(object => object._id == id && object.color == color); 
+  
+      if(index != -1){
+        //product is in the cart 
+        //check if color is the same
+        cart[index].quantity += parseInt(quantity); 
+        }else{
+        //check if color is the same
+        //check if color is the same
+        cart.push({_id:id,quantity: parseInt(quantity), color:color})
+      };
+  
+      //product not in cart 
+     //else{
+       //cart.push({_id:id,quantity:1, color:color})
+     }
+
+    
+//add item to the array
+localStorage.setItem('cart',JSON.stringify(cart));
+
+    alert('cart existing');
+  }
+
+  //then store
+
+
